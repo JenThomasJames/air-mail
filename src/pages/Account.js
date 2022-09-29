6279cc9cfb0c382b318e3e49a2ae051723645725
JenThomasJames <jenthomasjames123@gmail.com>
2022-09-29 15:30:29 +0530

    Added logout feature

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,2 +1,28 @@
-const Account = () => {};
+import styles from "./Account.module.css";
+import Card from "../components/Card";
+import { useNavigate } from "react-router-dom";
+const Account = () => {
+  const navigate = useNavigate();
+  const logoutHandler = () => {
+    localStorage.clear();
+    navigate("/auth");
+  };
+
+  return (
+    <div className={styles.flex}>
+      <Card>
+        <div className={styles["card-wrapper"]}>
+          <div className={styles["card-header"]}>
+            <p className={styles["text-muted"]}>
+              {localStorage.getItem("loggedInUserId")}
+            </p>
+            <button onClick={logoutHandler} className={styles["logout-btn"]}>
+              Logout
+            </button>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+};
 export default Account;
